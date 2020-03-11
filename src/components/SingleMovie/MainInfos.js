3eb9c904a73d4af79dadaa1620ceceb37def3a0b
Nicolas DISANT <n.disant@hotmail.com>
2020-03-11 17:47:27 +0100

    add modal video for trailers + add animation on ratings

--- src/components/SingleMovie/MainInfos.js
+++ src/components/SingleMovie/MainInfos.js
@@ -1,25 +1,57 @@
 import React, { Component } from "react";
 import play from "./../../assets/play.svg";
+import ModalVideo from "react-modal-video";
+
+/* Animated progress bar */
+import { easeQuadInOut } from "d3-ease";
+import { CircularProgressbarWithChildren, buildStyles } from "react-circular-progressbar";
+import AnimatedProgressProvider from "./../../utils/AnimatedProgressProvider";
+import "react-circular-progressbar/dist/styles.css";
 
 export default class MainInfos extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        };
+    }
+    openModal = () => {
+        this.setState({ isOpen: true });
+    };
     render() {
         const { datas } = this.props;
         return (
             <div>
                 <h1>{datas.title}</h1>
                 <div className="moreInfos">
-                    <div className="rating">{datas.vote_average * 10} %</div>
+                    <AnimatedProgressProvider valueStart={0} valueEnd={datas.vote_average * 10} duration={1.4} easingFunction={easeQuadInOut}>
+                        {(value) => {
+                            const displayRating =
+                                value > 0 ? (
+                                    <span>
+                                        {Math.round(value)} <sup>%</sup>
+                                    </span>
+                                ) : (
+                                    "NR"
+                                );
+                            return (
+                                <CircularProgressbarWithChildren className="rating" value={value} styles={buildStyles({ pathTransition: "none" })}>
+                                    <p className="ratingText">{displayRating}</p>
+                                </CircularProgressbarWithChildren>
+                            );
+                        }}
+                    </AnimatedProgressProvider>
                     {datas.videos && datas.videos.results.length > 0 ? (
                         <div className="video">
-                            <a
-                                href={`https://www.youtube.com/watch?v=${datas.videos.results[0].key}`}
-                                target="_blank"
-                                className="trailer"
-                                rel="noopener noreferrer"
-                            >
-                                <img src={play} alt="logoPlay" className="playTrailer" />
-                                Voir la bande annonce
-                            </a>
+                            <ModalVideo
+                                channel="youtube"
+                                isOpen={this.state.isOpen}
+                                videoId={datas.videos.results[0].key}
+                                onClose={() => this.setState({ isOpen: false })}
+                            />
+                            <button className="trailer" onClick={this.openModal}>
+                                <img src={play} alt="logoPlay" className="playTrailer" /> Play Trailer
+                            </button>
                         </div>
                     ) : null}
                 </div>
